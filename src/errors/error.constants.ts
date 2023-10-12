9515219b807dce7a8da4fd43fadeb68399e9b847
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-10-13 03:00:36 +0530

    :sparkles: Initial Commit

---
+++ src/errors/error.constants.ts
@@ -0,0 +1,15 @@
+export const errors = {
+    MONGODB_CONNECTION_ERROR: {
+      errorCode: 500,
+      status: "MongoDB Connect Error!",
+    },
+    USER_ALREADY_EXIST: {
+      errorCode: 409,
+      status: "User Already Exist!",
+    },
+    USER_NOT_FOUND: {
+      errorCode: 404,
+      status: "User Does not Exist!",
+    },
+  };
+  
